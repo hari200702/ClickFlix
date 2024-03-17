9db09d9a99d74f68b9c8623e35315e0359defa30
hari200702 <116946126+hari200702@users.noreply.github.com>
2024-03-17 20:52:27 +0530

    Changes

---
+++ ClickFlix/src/components/watch/SinglePage.jsx
@@ -0,0 +1,59 @@
+import React, { useState, useEffect } from "react"
+import "./style.css"
+import { useParams } from "react-router-dom"
+import { homeData, recommended, trending } from "../../dummyData"
+import Upcomming from "../upcoming/Upcomming"
+
+const SinglePage = () => {
+  const { id } = useParams()
+  const [item, setItem] = useState(null)
+
+  useEffect(() => {
+    let item = homeData.find((item) => item.id === parseInt(id))
+    if (item) {
+      setItem(item)
+    }
+  }, [id])
+  useEffect(() => {
+    let item = trending.find((item) => item.id === parseInt(id))
+    if (item) {
+      setItem(item)
+    }
+  }, [id])
+  const [rec, setRec] = useState(recommended)
+
+  return (
+    <>
+      {item ? (
+        <>
+          <section className='singlePage'>
+            <div className='singleHeading'>
+              <h1>{item.name} </h1> <span> | {item.time} | </span> <span> HD </span>
+            </div>
+            <div className='container'>
+              <video src={item.video} controls></video>
+              <div className='para'>
+                <h3>Date : {item.date}</h3>
+                <p>{item.desc}</p>
+                <p></p>
+                <p></p>
+                <p></p>
+              </div>
+              <div className='soical'>
+                <h3>Share : </h3>
+                <img src='https://img.icons8.com/color/48/000000/facebook-new.png' />
+                <img src='https://img.icons8.com/fluency/48/000000/twitter-circled.png' />
+                <img src='https://img.icons8.com/fluency/48/000000/linkedin-circled.png' />
+              </div>
+            </div>
+          </section>
+          <Upcomming items={rec} title='Recommended Movies' />
+        </>
+      ) : (
+        "no"
+      )}
+    </>
+  )
+}
+
+export default SinglePage
